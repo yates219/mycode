3cb6051dc6084fbc3f9a4c2e98101a9aa9920542
Colin Yates <colinyates@users.noreply.github.com>
2022-02-28 18:47:49 +0000

    Learning to track ISS with requests

---
+++ iss/requests-ride_iss-chall.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""tracking the iss using
+   api.open-notify.org/astros.json | Alta3 Research"""
+
+# notice we no longer need to import urllib.request or json
+import requests
+
+## Define URL
+MAJORTOM = 'http://api.open-notify.org/astros.json'
+
+def main():
+    """runtime code"""
+
+    ## Call the webservice
+    groundctrl = requests.get(MAJORTOM)
+    
+    # Convert into dictionary
+    astronauts = groundctrl.json()
+
+    for x in astronauts['people']:
+        print(f"{x['name']} is on the {x['craft']}")
+
+if __name__ == "__main__":
+    main()
+
